1467c2994b9958f30e16dac3cb996f6e362d8b48
HammedBabatunde <olawale6708@gmail.com>
2022-04-19 21:08:08 +0100

    Updated main.h

--- main.h
+++ main.h
@@ -1,16 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-/**
- * struct mystr - pointer to function with corresponding letter
- * @letter: specifier
- * @func: print argument
- */
-typedef struct mystr
-{
-	char *letter;
-	int (*func)(va_list);
-} mystr;
-int _putchar(char c);
 
-#endif
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+        int plus;
+        int space;
+        int hash;
+} flags_t;
+
+/* _printf */
+int _printf(const char *format, ...);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
