4b06eb00e288f6b6c9f52efcf7e0857c52f16727
Oluwatobi-M <michaelblake1689@gmail.com>
2022-04-19 21:57:26 +0100

    update header file

--- main.h
+++ main.h
@@ -28,6 +28,9 @@ int _puts(char *str);
 /*converter*/
 char *convert(unsigned long int num, int base, int lowercase);
 
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
+
 /* get_flag */
 int get_flag(char s, flags_t *f);
 
