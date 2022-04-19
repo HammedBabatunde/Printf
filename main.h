644edc0dfb4ca12a3fac88b6871062532d4e01ab
Hammed Babatunde <55561306+HammedBabatunde@users.noreply.github.com>
2022-04-19 15:07:28 -0700

    Update main.h

--- main.h
+++ main.h
@@ -18,6 +18,7 @@ typedef struct flags
         int hash;
 } flags_t;
 
+
 /**
  * struct printHandler - struct to choose the right function depending
  * on the format specifier passed to _printf()
@@ -26,8 +27,8 @@ typedef struct flags
  */
 typedef struct printHandler
 {
-	char c;
-	int (*f)(va_list ap, flags_t *f);
+        char c;
+        int (*f)(va_list ap, flags_t *f);
 } ph;
 
 /* print_nums */
@@ -36,21 +37,17 @@ void print_number(int n);
 int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
 
-/* _printf */
-int _printf(const char *format, ...);
-
-/* write_funcs */
-int _putchar(char c);
-int _puts(char *str);
-
 /* print_bases */
 int print_hex(va_list l, flags_t *f);
 int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
 int print_octal(va_list l, flags_t *f);
 
-/*converter*/
+/* converter */
 char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
+int _printf(const char *format, ...);
 
 /* get_print */
 int (*get_print(char s))(va_list, flags_t *);
@@ -58,17 +55,20 @@ int (*get_print(char s))(va_list, flags_t *);
 /* get_flag */
 int get_flag(char s, flags_t *f);
 
-/* print_address */
-int print_address(va_list l, flags_t *f);
-
 /* print_alpha */
 int print_string(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
 
 /* print_custom */
 int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
+
+/* print_address */
+int print_address(va_list l, flags_t *f);
 
 /* print_percent */
 int print_percent(va_list l, flags_t *f);
